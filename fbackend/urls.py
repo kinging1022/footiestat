ff19b040456e302d80342791325089b120df4ad6
Emmanuel Ighoruemuse <emmanuelemmy0906@gmail.com>
2026-04-07 00:58:00 +0100

    added robot.txt

--- fbackend/urls.py
+++ fbackend/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
 from football.sitemaps import StaticSitemap, FixtureSitemap
 from blog.sitemaps import BlogHomeSitemap, ArticleSitemap
 
@@ -15,9 +16,20 @@ sitemaps = {
     'articles': ArticleSitemap,
 }
 
+def robots_txt(request):
+    content = (
+        "User-agent: *\n"
+        "Allow: /\n"
+        "Disallow: /fst-manage-2026/\n"
+        "\n"
+        "Sitemap: https://footiestat.com/sitemap.xml\n"
+    )
+    return HttpResponse(content, content_type='text/plain')
+
 urlpatterns = [
     path('fst-manage-2026/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
     path('', include('football.urls')),
     path('', include('pages.urls')),
     path('', include('blog.urls')),
